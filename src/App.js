87976a816a3cf8a18ca75b23e33bc3c0d46edd2f
kitou88C39 <kanfutrooper@icloud.com>
2023-03-05 14:39:20 +0900

    useState useEffect設定

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import './App.css';
 
 import { AgGridReact } from 'ag-grid-react';
+import { useState, useEffect } from 'react';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 function App() {
   //データ用の配列と列定義の配列を作成する
-  const rowData = [
+  const [rowData, setRowData] = useState([
     { make: 'Ford', model: 'Focus', price: 40000 },
     { make: 'Toyota', model: 'Celica', price: 45000 },
     { make: 'BMW', model: '4 series', price: 50000 },
-  ];
+  ]);
 
-  const cloumnDefs = [
+  const [columnDefs, serColumnDefs] = useState([
     { field: 'make' },
     { field: 'model' },
     { field: 'price' },
-  ];
+  ]);
+
+  useEffect(() => {
+    fetch(
+      'https://www.ag-grid.com/example-assets/row-data.json'
+        .then((result) => result.json())
+        .then((rowData) => setRowData(rowData)),
+      []
+    );
+  });
 
   return (
     // divタグでcssのクラスとテーマを設定する
     <div className='ag-theme-alpine' style={{ height: 500 }}>
       {/* Grid行にデータと列定義のプロパティに設定する */}
-      <AgGridReact rowData={rowData} cloumnDefs={cloumnDefs} />
+      <AgGridReact rowData={rowData} columnDefs={columnDefs} />
     </div>
   );
 }
